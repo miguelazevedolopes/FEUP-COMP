05fec0d54cfed31d1d5307f55cdcb1b8364be10d
up201908209 <up201908209@fc.up.pt>
2022-03-31 14:09:06 +0100

    recursion starting to stop

--- test/ExampleTest.java
+++ test/ExampleTest.java
@@ -6,7 +6,7 @@ public class ExampleTest {
 
     @Test
     public void testExpression() {
-        TestUtils.parse("2+3\n10+20\n");
+        //TestUtils.parse("2+3\n10+20\n");
         // var parserResult = TestUtils.parse("2+3\n10+20\n");
         // parserResult.getReports().get(0).getException().get().printStackTrace();
         // // System.out.println();
