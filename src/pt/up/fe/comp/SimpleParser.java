b9ede0791616d0e32f7ae83e4a2c7ef175cf23c1
up201908209 <up201908209@fc.up.pt>
2022-03-31 21:34:44 +0100

    recursion solved, i think

--- src/pt/up/fe/comp/SimpleParser.java
+++ src/pt/up/fe/comp/SimpleParser.java
@@ -44,12 +44,12 @@ public class SimpleParser implements JmmParser {
 
             return new JmmParserResult((JmmNode) root, Collections.emptyList(), config);
 
-        } catch(ParseException p){
+        /*} catch(ParseException p){
             int lineError = e.getStackTrace()[0].getLineNumber();
             String message = e.getMessage();
-            /* reportList.add(new Report(ReportType.ERROR, Stage.SYNTATIC, lineError, message));
-            return new JmmParserResult(null, reportList); */
-            return JmmParserResult.newError(Report.newError(Stage.LEXICAL, -1, -1, message, p));
+            reportList.add(new Report(ReportType.ERROR, Stage.SYNTATIC, lineError, message));
+            return new JmmParserResult(null, reportList);
+            return JmmParserResult.newError(Report.newError(Stage.LEXICAL, -1, -1, message, p));*/
         } catch (Exception e) {
             return JmmParserResult.newError(Report.newError(Stage.SYNTATIC, -1, -1, "Exception during parsing", e));
         } 
