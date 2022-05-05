7e980e97b49fa57ff6fe4c6875f5447de99c12f4
up201908209 <up201908209@fc.up.pt>
2022-05-05 16:06:19 +0100

    some problems with libraries

--- src/pt/up/fe/comp/ollir/JmmOptmizer.java
+++ src/pt/up/fe/comp/ollir/JmmOptmizer.java
@@ -2,10 +2,13 @@ package pt.up.fe.comp.ollir;
 
 import java.util.Collections;
 import pt.up.fe.comp.jmm.analysis.table.SymbolTable;
-import pt.up.fe.comp.jmm.astAJmmVisitor;
+import pt.up.fe.comp.jmm.ast.AJmmVisitor;
 import pt.up.fe.comp.jmm.ast.JmmNode;
+import pt.up.fe.comp.jmm.ollir.JmmOptimization;
+import pt.up.fe.comp.jmm.analysis.JmmSemanticsResult;
+import pt.up.fe.comp.jmm.ollir.OllirResult;
 
-public class JmmOptimizer implements JmmOptimization{
+public class JmmOptmizer implements JmmOptimization{
     public OllirResult toOllir(JmmSemanticsResult semanticsResult){
         var ollirGenerator = new OllirGenerator(SemanticsResult.getSymbolTable());
         ollirGenerator.visit(SemanticsResult.getRootNode());
